3c0ae31964cba1d7178064af6ed2489bbea80371
Carlos F. Martínez Sánchez <carlos.f.martinez.s+github@gmail.com>
2017-06-04 14:06:35 -0400

    Configuracion servicio mediante anotaciones
    Se agrega anotacion de servicio
    Se quita servicio de applicationContext.xml

--- src/main/java/cl/cmartinezs/marifer/services/impl/IndexServiceImpl.java
+++ src/main/java/cl/cmartinezs/marifer/services/impl/IndexServiceImpl.java
@@ -1,12 +1,14 @@
 package cl.cmartinezs.marifer.services.impl;
 
+import org.springframework.stereotype.Service;
+
 import cl.cmartinezs.marifer.services.IndexService;
 
+@Service("indexService")
 public class IndexServiceImpl implements IndexService {
 
 	@Override
 	public String getMessage() {
 		return "HOLA MUNDO! desde un servicio de Spring Framework";
 	}
-
 }
