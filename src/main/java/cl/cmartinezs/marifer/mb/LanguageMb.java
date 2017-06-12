a3a7a2ab8124b13a1cdd2819998e1f87d9586844
Carlos F. Martínez Sánchez <carlos.f.martinez.s+github@gmail.com>
2017-06-11 21:47:48 -0400

    Internacionalización
    Configuración basica para la internacionalización, idioma por defecto
    español y secundario inglés

---
+++ src/main/java/cl/cmartinezs/marifer/mb/LanguageMb.java
@@ -0,0 +1,32 @@
+package cl.cmartinezs.marifer.mb;
+
+import java.io.Serializable;
+import java.util.Locale;
+
+import javax.faces.context.FacesContext;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Scope
+public class LanguageMb implements Serializable {
+
+	private static final long serialVersionUID = -8237470747508561119L;
+	
+	private Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+
+	public Locale getLocale() {
+		return locale;
+	}
+	
+	public String getLanguage(){
+		return locale.getLanguage();
+	}
+	
+	public void changeLanguage(String language){
+		locale = new Locale(language);
+		FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+	}
+
+}
